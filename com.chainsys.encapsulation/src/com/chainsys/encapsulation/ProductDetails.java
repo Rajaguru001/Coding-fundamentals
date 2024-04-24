15a794e5f210a4f9864be1e884020280183b64b4
Raja3561 <Raja3561@CSL0704.global.chainsys.com>
2024-04-24 17:11:50 +0530

    product pricing 1

--- com.chainsys.encapsulation/src/com/chainsys/encapsulation/ProductDetails.java
+++ com.chainsys.encapsulation/src/com/chainsys/encapsulation/ProductDetails.java
@@ -99,7 +99,7 @@ public class ProductDetails {
 
 		System.out.println(" The Products Are ");
 		System.out.println("-------------------------------");
-		System.out.println("-->1) Amazon" + "\n" + "-->2) flipkart" + "\n" + "-->3) Ajio");
+		System.out.println("-->1) gps console" + "\n" + "-->2) gaming console" + "\n" + "-->3) smart home theater");
 		System.out.println("-------------------------------");
 		System.out.println("Please choose the Number 1,2,3");
 		System.out.println("-------------------------------");
@@ -110,8 +110,8 @@ public class ProductDetails {
 			case "1":
 				int amazonwarrenty = 5;
 				bp.setNameofproduct(nameofproduct.toLowerCase());
-				System.out.println("product: Amazon");
-				pi.amazon(nameofproduct, 12000, "2M", 5, amazonwarrenty);
+				System.out.println("product: gps console");
+				pi.amazon(nameofproduct, 50000, "2M", 5, amazonwarrenty);
 				System.out.println("Warrenty : " + productwarranty(amazonwarrenty));
 				System.out.println("------------------------------------");
 				System.out.println("Thank you for purchasing our product");
@@ -124,8 +124,8 @@ public class ProductDetails {
 			case "2":
 				int flipkartwarranty = 3;
 				bp.setNameofproduct(nameofproduct);
-				System.out.println("product: Flipkart");
-				pi.flipkart(nameofproduct, 13000, "1.5M", 4.5, flipkartwarranty);
+				System.out.println("product: gaming console");
+				pi.flipkart(nameofproduct, 60000, "1.5M", 4.5, flipkartwarranty);
 				System.out.println("Warranty :" + productwarranty(flipkartwarranty));
 				System.out.println("------------------------------------");
 				System.out.println("Thank you for purchasing our product");
@@ -135,8 +135,8 @@ public class ProductDetails {
 			case "3":
 				int ajio = 4;
 				bp.setNameofproduct(nameofproduct);
-				System.out.println("product: Ajio");
-				pi.Ajio(nameofproduct, 14000, "500k", 4, ajio);
+				System.out.println("product: Smart Home Theater");
+				pi.Ajio(nameofproduct, 40000, "500k", 4, ajio);
 				System.out.println("Warranty: " + productwarranty(ajio));
 				System.out.println("------------------------------------");
 				System.out.println("Thank you for purchasing our product");
@@ -164,6 +164,7 @@ public class ProductDetails {
 		}
 		System.out.println("do have membership (yes/no)?");
 		String m = scn.next();
+		
 		System.out.println("----------------------------------------------");
 		System.out.println("*Congrats you have now 1 year !Extra warranty*");
 		if (m.equalsIgnoreCase("yes")) {
@@ -176,29 +177,29 @@ public class ProductDetails {
 
 	public static int amazonwarrentyy() {
 		Scanner product = new Scanner(System.in);
-		System.out.println("please choose the product(App,Amazon prime,Amazon smart home)");
+		System.out.println("please choose the product(Navigator,Location Finder,Smart way out)");
 
 		boolean b = true;
 		while (b) {
 			String product1 = product.next();
 
 			switch (product1.toLowerCase()) {
-			case "app":
-				System.out.println("The cost is 400000");
+			case "navigator":
+				System.out.println("The cost is 15000");
 				System.out.println("Date of release:Next month");
 				System.out.println("pre Register: 20k");
 				System.out.println("Special discount for Members");
 				b = false;
 				break;
-			case "amazonprime":
-				System.out.println("The cost is 600000");
+			case "location finder":
+				System.out.println("The cost is 18000");
 				System.out.println("Date of release:Next month");
 				System.out.println("pre Register: 10k");
 				System.out.println("Special discount for Members");
 				b = false;
 				break;
-			case "amazonsmarthome":
-				System.out.println("The cost is 800000");
+			case "smart way out":
+				System.out.println("The cost is 17000");
 				System.out.println("Date of release:Not yet confirmed");
 				System.out.println("pre Register:1k");
 				System.out.println("Special discount for Members");
@@ -218,27 +219,27 @@ public class ProductDetails {
 
 	public static int flipkartwarrentyy() {
 		Scanner product = new Scanner(System.in);
-		System.out.println("please choose the product (APP,Flipkartshow,flipkart smart home)");
+		System.out.println("please choose the product (Gaming fps,Bluestack,Online games");
 		boolean b = true;
 		while (b) {
 			String product1 = product.next();
 			switch (product1.toLowerCase()) {
-			case "app":
-				System.out.println("The cost is 300000");
+			case "gaming fps":
+				System.out.println("The cost is 20000");
 				System.out.println("Date of release:Next month");
 				System.out.println("pre Register: 21k");
 				System.out.println("Special discount for Members");
 				b = false;
 				break;
-			case "flipkartshow":
-				System.out.println("The cost is 600000");
+			case "bluestack":
+				System.out.println("The cost is 10000");
 				System.out.println("Date of release:Next month");
 				System.out.println("pre Register: 12k");
 				System.out.println("Special discount for Members");
 				b = false;
 				break;
-			case "flipkart smart home":
-				System.out.println("The cost is 800000");
+			case "online games":
+				System.out.println("The cost is 25000");
 				System.out.println("Date of release:Not yet confirmed");
 				System.out.println("pre Register:5k");
 				System.out.println("Special discount for Members");
@@ -257,27 +258,27 @@ public class ProductDetails {
 
 	public static int ajiowarrenty() {
 		Scanner product = new Scanner(System.in);
-		System.out.println("please choose the product (AjioaPP,Jio Tv, Jio smart home)");
+		System.out.println("please choose the product (Streaming movie,Rented Movies,dolby home driver");
 		boolean b = true;
 		while (b) {
 			String product1 = product.next();
 			switch (product1.toLowerCase()) {
-			case "ajioapp":
-				System.out.println("The cost is 400000");
+			case "streaming":
+				System.out.println("The cost is 10000");
 				System.out.println("Date of release:Next month");
 				System.out.println("pre Register: 26k");
 				System.out.println("Special discount for Members");
 				b = false;
 				break;
-			case "jio tv":
-				System.out.println("The cost is 500000");
+			case "rented movies":
+				System.out.println("The cost is 12000");
 				System.out.println("Date of release:Next month");
 				System.out.println("pre Register: 13k");
 				System.out.println("Special discount for Members");
 				b = false;
 				break;
-			case "jio smart home":
-				System.out.println("The cost is 600000");
+			case "dolby home driver":
+				System.out.println("The cost is 14000");
 				System.out.println("Date of release:Not yet confirmed");
 				System.out.println("pre Register :5k");
 				System.out.println("Special discount for Members");
